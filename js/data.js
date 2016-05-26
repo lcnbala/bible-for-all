f20a2c4c6ee5a44373d5af24acf2adfcc74686d7
domnulnopcea <domnulnopcea@gmail.com>
2016-05-26 18:00:52 +0300

    cleanups

--- js/data.js
+++ js/data.js
@@ -220,12 +220,4 @@ $(document).ready(function() {
 	});	
 	
 	$('#bible').val(localStorage['bible_version']);
-
-	var $window = $(window),
-	$stickyEl = $('#the-sticky-div'),
-	elTop = $stickyEl.offset().top;
-
-	$window.scroll(function() {
-		$stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-	});
 });
